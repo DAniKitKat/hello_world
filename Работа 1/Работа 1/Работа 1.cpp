b20e2446f8dba84fd74c0f0eaff23bf78102ff2b
KitKatLazyKat <141255770+DAniKitKat@users.noreply.github.com>
2025-12-19 09:09:16 +0300

    1 работа
    публикация 1 работы

---
+++ Работа 1/Работа 1/Работа 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+int sum(int A, int B) {
+	return A + B;
+}
+
+int main() {
+	setlocale(0, "Russian");
+	int a = 0, b = 0;
+	cout << "Выберете значение переменных a и b:";
+	cin >> a >> b;
+	cout << sum(a, b);
+}
